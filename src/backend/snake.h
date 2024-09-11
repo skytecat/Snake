cd27a245af923ea53e76157aa0baddd61f5c6654
Alyona <ignatavichute@gmail.com>
2024-09-11 09:43:35 +0300

    Create snake.h

---
+++ src/backend/snake.h
@@ -0,0 +1,16 @@
+#ifndef _BRICK_GAME_SNAKE_SNAKE_H_
+#define _BRICK_GAME_SNAKE_SNAKE_H_
+
+#include <ncurses.h>
+
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <deque>
+#include <fstream>
+#include <iostream>
+
+#define WIDTH 10
+#define HEIGHT 20
+
+#endif
